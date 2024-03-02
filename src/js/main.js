74a2f01b6d14df8379cd318bba64f5ce44f0caab
Julien Houyet <julien.houyet@gmail.com>
2024-03-02 12:19:17 +0100

    refactor: Split logic between main.js and ui.js and add AC button functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,63 +1,27 @@
-import '../style.css';
+import { updateDisplay, appendNumberToDisplay, appendDecimal, resetDisplay } from './ui.js';
 
-// Select all buttons and the display element
-const numberButtons = document.querySelectorAll('.number');
-const decimalButton = document.querySelector('.decimal');
-const displayElement = document.querySelector('.display');
+// Initialize the calculator when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+	// Select all buttons and the display element
+	const displayElement = document.querySelector('.display');
+	const numberButtons = document.querySelectorAll('.number');
+	const decimalButton = document.querySelector('.decimal');
+	const clearButton = document.querySelector('.clear');
 
-// Update the display when a number button is clicked
-function appendNumberToDisplay(number) {
-	// Check if length already exceeds 19 characters
-	if (displayElement.textContent.length >= 19) return;
+	// Attach an event handler to each number button
+	numberButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			appendNumberToDisplay(button.textContent, displayElement);
+		});
+	});
 
-	// If '0' is displayed, replace it with the number clicked
-	// Else, add the number after
-	if (displayElement.textContent === '0') {
-		displayElement.textContent = number;
-	} else {
-		displayElement.textContent += number;
-	}
-	updateDisplay();
-}
+	// Attach event handler to the decimal button
+	decimalButton.addEventListener('click', () => {
+		appendDecimal(displayElement);
+	});
 
-// Append a decimal point to the display if it doesn't already contain one
-function appendDecimal() {
-	// Check if the length already exceeds 19 characters or if a ',' already exists
-	if (displayElement.textContent.length >= 19 || displayElement.textContent.includes(',')) return;
-
-	if (!displayElement.textContent.includes(',')) {
-		displayElement.textContent += ',';
-	}
-	updateDisplay();
-}
-
-// Attach an event handler to each number button
-numberButtons.forEach(button => {
-	button.addEventListener('click', () => {
-		appendNumberToDisplay(button.textContent);
+	// Attach event handler to the AC button
+	clearButton.addEventListener('click', () => {
+		resetDisplay(displayElement);
 	});
 });
-
-// Attach event handler to the decimal button
-decimalButton.addEventListener('click', appendDecimal);
-
-function updateDisplay() {
-	const text2xlLength = 9;
-	const textxlLength = 14;
-	const textlgLength = 17;
-	const currentLength = displayElement.textContent.length;
-
-	if (currentLength > textlgLength) {
-		// Pour un contenu moyennement long, utilisez une taille de police moyenne
-		displayElement.className = 'display text-lg';
-	} else if (currentLength > textxlLength) {
-		// Pour un contenu moyennement long, utilisez une taille de police moyenne
-		displayElement.className = 'display text-xl';
-	} else if (currentLength > text2xlLength) {
-		// Pour un contenu moyennement long, utilisez une taille de police moyenne
-		displayElement.className = 'display text-2xl';
-	} else {
-		// Pour un contenu court, utilisez la taille de police par défaut
-		displayElement.className = 'display text-4xl';
-	}
-}
