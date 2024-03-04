4dcf04eea225a1629e36de186a771e1a1e573dd7
Julien Houyet <julien.houyet@gmail.com>
2024-03-04 14:25:16 +0100

    docs: refactor comments for clarity and conciseness

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { Calculator } from './calculator.js';
 
-// Initialize the calculator when the DOM is fully loaded
+/** Initialize the calculator when the DOM is fully loaded */
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Select all buttons and the display element
@@ -13,44 +13,44 @@ document.addEventListener('DOMContentLoaded', () => {
 	const toggleSignButton = document.querySelector('.toggle-sign');
 	const operationButtons = document.querySelectorAll('.operation');;
 
-	// Creates a new instance of the Calculator class
+	/** Creates a new instance of the Calculator class */
 	const calculator = new Calculator(displayElement, operationButtons);
 
-	// Attach an event handler to each number button
+	/** Attach an event handler to each number button */
 	numberButtons.forEach(button => {
 		button.addEventListener('click', () => {
 			calculator.appendNumber(button.textContent);
 		});
 	});
 
-	// Attach event handler to the decimal button
+	/** Attach event handler to the decimal button */
 	decimalButton.addEventListener('click', () => {
 		calculator.appendDecimal();
 	});
 
-	// Attach event handler to the AC button
+	/** Attach event handler to the AC button */
 	clearButton.addEventListener('click', button => {
 		calculator.clear();
 	});
 
-	// Attach an event handler to each operation button
+	/** Attach an event handler to each operation button */
 	operationButtons.forEach(button => {
 		button.addEventListener('click', () => {
 			calculator.chooseOperation(button.getAttribute('data-operation'));
 		});
 	});
 
-	// Attach event handler to the percent button
+	/** Attach event handler to the percent button */
 	percentButton.addEventListener('click', () => {
 		calculator.applyPercentage();
 	});
 
-	// Attach event handler to the toggle sign button
+	/** Attach event handler to the toggle sign button */
 	toggleSignButton.addEventListener('click', () => {
 		calculator.applyReverseSign();
 	});
 
-	// Attach event handler to the equal button
+	/** Attach event handler to the equal button */
 	equalsButton.addEventListener('click', button => {
 		calculator.compute();
 		calculator.removeRingOperation();
